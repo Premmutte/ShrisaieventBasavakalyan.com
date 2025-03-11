ca262eae6773a0151c2db2190b642885712d17cf
Prem <152166408+Premmutte@users.noreply.github.com>
2025-03-11 11:23:33 +0530

    Add files via upload

---
+++ AIS/script.js
@@ -0,0 +1,6 @@
+// Handle contact form submission
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for contacting us! We will get back to you soon.");
+    this.reset();
+  });
